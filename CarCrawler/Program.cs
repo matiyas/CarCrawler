f942d6220676769ccee1b1e23b41a148470cd567
Damian Matyjaszek <tougepl@gmail.com>
2023-02-18 20:57:15 +0100

    Add AdListLinksScraperService

--- CarCrawler/Program.cs
+++ CarCrawler/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CarCrawler.Services.Scrapers;
+
+var adListLinksScraperService = new AdListLinksScraperService(@"https://www.otomoto.pl/osobowe/honda/civic");
+var links = adListLinksScraperService.Call();
+
+Console.WriteLine(string.Join("\n", links));
+
---
+++ CarCrawler/Services/Scrapers/AdListLinksScraperService.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+
+namespace CarCrawler.Services.Scrapers;
+
+internal class AdListLinksScraperService
+{
+    private const string AdXpath = "//main/article/div/h2/a";
+    private string _adListLink;
+
+    internal AdListLinksScraperService (string adListLink)
+    {
+        this._adListLink = adListLink;
+    }
+
+    internal IEnumerable<string> Call ()
+    {
+        var htmlNodes = GetHtmlNodes();
+
+        return GetLinksFromHtmlNodes(htmlNodes);
+    }
+
+    private IEnumerable<HtmlNode> GetHtmlNodes ()
+    {
+        var web = new HtmlWeb();
+        var htmlDoc = web.Load(_adListLink);
+
+        return htmlDoc.DocumentNode.SelectNodes(AdXpath);
+    }
+    private IEnumerable<string> GetLinksFromHtmlNodes(IEnumerable<HtmlNode> htmlNodes) => htmlNodes.Select(GetLinkFromHtmlNode).Where(attribute => attribute != null);
+    private string GetLinkFromHtmlNode(HtmlNode htmlNode) => htmlNode.Attributes.FirstOrDefault(attribute => attribute.Name == "href")?.Value!;
+}
